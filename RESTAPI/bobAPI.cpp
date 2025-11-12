55c66e38361b385595c093a253cb1409a9e6b3cb
krypdkat <39078779+krypdkat@users.noreply.github.com>
2025-11-12 22:10:59 +0700

    typo

--- RESTAPI/bobAPI.cpp
+++ RESTAPI/bobAPI.cpp
@@ -376,7 +376,7 @@ std::string bobGetEpochInfo(uint16_t epoch)
     uint32_t initTick = 0;
     db_get_u32("end_epoch_tick:" + es, end_epoch_tick);
     db_get_end_epoch_log_range(epoch, start, length);
-    db_insert_u32("init_tick:"+std::to_string(epoch), initTick);
+    db_get_u32("init_tick:"+std::to_string(epoch), initTick);
 
     root["epoch"] = epoch;
     root["initialTick"] = initTick;
