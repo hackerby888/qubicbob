f5bcc0dd2dc620ded08dd7550090ac8b731eb06b
krypdkat <39078779+krypdkat@users.noreply.github.com>
2025-11-20 11:47:49 +0700

    fix a bug in indexer

--- QubicIndexer.cpp
+++ QubicIndexer.cpp
@@ -66,7 +66,7 @@ static void indexTick(uint32_t tick, const TickData &td) {
                     std::vector<uint8_t> tx_data;
                     if (db_try_get_transaction(txHash, tx_data)) {
                         auto tx = (Transaction*)tx_data.data();
-                        if (tx->amount <= gSpamThreshold && tx->inputSize == 0 && tx->inputType == 0) // spam tx => not index
+                        if (tx->amount < gSpamThreshold && tx->inputSize == 0 && tx->inputType == 0) // spam tx => not index
                         {
                             continue;
                         }
